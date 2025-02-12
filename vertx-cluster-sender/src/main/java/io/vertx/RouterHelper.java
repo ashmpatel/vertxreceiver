5c95db8b9e7ae36daf7245510b7b6df8cc85953a
Ashish Patel <ashmpatel@gmail.com>
2025-02-12 15:22:59 +0000

    Update RouterHelper.java

--- vertx-cluster-sender/src/main/java/io/vertx/RouterHelper.java
+++ vertx-cluster-sender/src/main/java/io/vertx/RouterHelper.java
@@ -2,6 +2,8 @@ package io.vertx;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.healthchecks.HealthCheckHandler;
+import io.vertx.ext.healthchecks.Status;
 import io.vertx.ext.web.Router;
 
 import static io.vertx.Constants.CONTENT_TYPE;
@@ -25,10 +27,25 @@ public class RouterHelper
      */
     public static Router createRouter(final Vertx vertx, final String welcomeMessage){
         final Router router = Router.router(vertx);
+
+        HealthCheckHandler healthCheckHandler1 = HealthCheckHandler.create(vertx);
+
         router.route("/").handler(routingContext -> {
             HttpServerResponse response = routingContext.response();
             response.putHeader(CONTENT_TYPE, HTML_PRODUCE).end("<h1>" + welcomeMessage + "</h1>");
         });
+
+        router.get("/health*").handler(healthCheckHandler1);
+
+        // for lomger checks
+//        healthCheckHandler1.register("my-procedure-name", promise -> {
+//            // Do the check ....
+//            // Upon success do
+//            promise.complete(Status.OK());
+//            // In case of failure do:
+//            promise.complete(Status.KO());
+//        });
+
         return router;
     }
 }
